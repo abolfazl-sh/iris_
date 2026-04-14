f2a69ba48c5a90b87b41325b968ba2a2534a5483
Abolfazl Sharifi <abolfazlph6@gmail.com>
2026-04-15 09:58:29 +1000

    counter example based on iris doc

--- count.v
+++ count.v
@@ -1,2 +1,258 @@
-From iris.base_logic Require Import iprop.
-From iris.proofmode Require Import proofmode.
+From iris.algebra Require Export auth excl frac numbers.
+From iris.base_logic.lib Require Export invariants.
+From iris.heap_lang Require Import lang proofmode notation par.
+
+
+Definition mk_counter : val :=
+  λ: <>, ref #0.
+
+Definition incr : val :=
+  rec: "incr" "c" :=
+  let: "n" := !"c" in
+  let: "m" := "n" + #1 in
+  if: CAS "c" "n" "m" then "n" else "incr" "c".
+
+Definition read : val :=
+  λ: "c", !"c".
+
+Module spec1.
+Section spec1.
+Context `{heapGS Σ}.
+
+Context `{!inG Σ (authR max_natUR)}.
+Let N := nroot .@ "counter".
+
+Definition is_counter (v : val) (γ : gname) (n : nat) : iProp Σ :=
+  ∃ l : loc, ⌜v = #l⌝ ∗ own γ (◯ MaxNat n) ∗
+    inv N (∃ m : nat, l ↦ #m ∗ own γ (● MaxNat m)).
+
+Global Instance is_counter_persistent v γ n :
+  Persistent (is_counter v γ n) := _.
+
+Lemma alloc_initial_state :
+  ⊢ |==> ∃ γ, own γ (● MaxNat 0) ∗ own γ (◯ MaxNat 0).
+Proof.
+  setoid_rewrite <- own_op.
+  apply own_alloc.
+  apply auth_both_valid_discrete.
+  split.
+  - apply max_nat_included; simpl.
+    reflexivity.
+  - cbv.
+    done.
+Qed.
+
+Lemma state_valid γ n m :
+  own γ (● MaxNat n) -∗
+  own γ (◯ MaxNat m) -∗
+  ⌜m ≤ n⌝.
+Proof.
+  iIntros "Hγ Hγ'".
+  iPoseProof (own_valid_2 with "Hγ Hγ'") as "%H".
+  iPureIntro.
+  apply auth_both_valid_discrete in H.
+  destruct H as [H _].
+  apply max_nat_included in H; cbn in H.
+  done.
+Qed.
+
+Lemma update_state γ n :
+  own γ (● MaxNat n) ==∗
+  own γ (● MaxNat (S n)) ∗ own γ (◯ MaxNat (S n)).
+Proof.
+  iIntros "H".
+  rewrite <- own_op.
+  iApply (own_update with "H").
+  apply auth_update_alloc.
+  apply max_nat_local_update; cbn.
+  by apply le_S.
+Qed.
+
+Lemma mk_counter_spec :
+  {{{ True }}} mk_counter #() {{{ c γ, RET c; is_counter c γ 0}}}.
+Proof.
+Admitted.
+
+Lemma read_spec c γ n :
+  {{{ is_counter c γ n }}} read c {{{ (u : nat), RET #u; ⌜n ≤ u⌝ }}}.
+Proof.
+Admitted.
+
+Lemma incr_spec c γ n :
+  {{{ is_counter c γ n }}}
+    incr c
+  {{{ (u : nat), RET #u; ⌜n ≤ u⌝ ∗ is_counter c γ (S n) }}}.
+Proof.
+  iIntros "%Φ (%l & -> & #Hγ' & #HI) HΦ".
+  iLöb as "IH".
+  wp_rec.
+  wp_bind (! _)%E.
+  iInv "HI" as "(%m & Hl & Hγ)".
+  wp_load.
+  iModIntro.
+  iSplitL "Hl Hγ".
+  { iExists m. iFrame. }
+  wp_pures.
+  wp_bind (CmpXchg _ _ _).
+  iInv "HI" as "(%m' & Hl & Hγ)".
+  destruct (decide (#m = #m')) as [e | ne].
+  - wp_cmpxchg_suc.
+    injection e as e.
+    apply (inj Z.of_nat) in e.
+    subst m'.
+Admitted.
+
+Context `{!spawnG Σ}.
+
+Lemma par_incr :
+  {{{ True }}}
+    let: "c" := mk_counter #() in
+    (incr "c" ||| incr "c");;
+    read "c"
+  {{{ n, RET #(S n); True }}}.
+Proof.
+Admitted.
+
+End spec1.
+End spec1.
+
+Module spec2.
+Section spec2.
+
+Context `{!heapGS Σ, !inG Σ (authR (optionUR (prodR fracR natR)))}.
+
+Let N := nroot .@ "counter".
+
+Definition is_counter (v : val) (γ : gname) (n : nat) (q : Qp) : iProp Σ :=
+  ∃ l : loc, ⌜v = #l⌝ ∗ own γ (◯ Some (q, n)) ∗
+    inv N (∃ m : nat, l ↦ #m ∗ own γ (● Some (1%Qp, m))).
+
+Lemma is_counter_add (c : val) (γ : gname) (n m : nat) (p q : Qp) :
+  is_counter c γ (n + m) (p + q) ⊣⊢ is_counter c γ n p ∗ is_counter c γ m q.
+Proof.
+  iSplit.
+  - iIntros "(%l & -> & [Hγ1 Hγ2] & #I)".
+    iSplitL "Hγ1".
+    + iExists l.
+      iSplitR; first done.
+      by iFrame.
+    + iExists l.
+      iSplitR; first done.
+      by iFrame.
+  - iIntros "[(%l & -> & Hγ1 & I) (%l' & %H & Hγ2 & _)]".
+    injection H as <-.
+    iExists l.
+    iSplitR; first done.
+    iCombine "Hγ1 Hγ2" as "Hγ".
+    iFrame.
+Qed.
+
+Lemma alloc_initial_state :
+  ⊢ |==> ∃ γ, own γ (● Some (1%Qp, 0)) ∗ own γ (◯ Some (1%Qp, 0)).
+Proof.
+  setoid_rewrite <-own_op.
+  iApply own_alloc.
+  apply auth_both_valid_discrete.
+  split.
+  - reflexivity.
+  - apply Some_valid.
+    apply pair_valid.
+    split.
+    + apply frac_valid.
+      reflexivity.
+    + cbv.
+      done.
+Qed.
+
+Lemma state_valid γ (n m : nat) (q : Qp) :
+  own γ (● Some (1%Qp, n)) -∗
+  own γ (◯ Some (q, m)) -∗
+  ⌜m ≤ n⌝.
+Proof.
+  iIntros "Hγ Hγ'".
+  iPoseProof (own_valid_2 with "Hγ Hγ'") as "%H".
+  iPureIntro.
+  apply auth_both_valid_discrete in H.
+  destruct H as [H _].
+  apply Some_pair_included in H.
+  destruct H as [_ H].
+  rewrite Some_included_total in H.
+  apply nat_included in H.
+  done.
+Qed.
+
+Lemma state_valid_full γ (n m : nat) :
+  own γ (● Some (1%Qp, n)) -∗
+  own γ (◯ Some (1%Qp, m)) -∗
+  ⌜m = n⌝.
+Proof.
+  iIntros "Hγ Hγ'".
+  iPoseProof (own_valid_2 with "Hγ Hγ'") as "%H".
+  iPureIntro.
+  apply auth_both_valid_discrete in H.
+  destruct H as [H _].
+  apply Some_included_exclusive in H.
+  - destruct H as [_ H]; cbn in H.
+    apply leibniz_equiv in H.
+    done.
+  - apply _.
+  - done.
+Qed.
+
+Lemma update_state γ n m (q : Qp) :
+  own γ (● Some (1%Qp, n)) ∗ own γ (◯ Some (q, m)) ==∗
+  own γ (● Some (1%Qp, S n)) ∗ own γ (◯ Some (q, S m)).
+Proof.
+  iIntros "H".
+  rewrite -!own_op.
+  iApply (own_update with "H").
+  apply auth_update.
+  apply option_local_update.
+  apply prod_local_update_2.
+  apply (op_local_update_discrete _ _ 1).
+  done.
+Qed.
+
+Lemma mk_counter_spec :
+  {{{ True }}} mk_counter #() {{{ c γ, RET c; is_counter c γ 0 1}}}.
+Proof.
+Admitted.
+
+Lemma read_spec (c : val) (γ : gname) (n : nat) (q : Qp) :
+  {{{ is_counter c γ n q }}}
+    read c
+  {{{ (u : nat), RET #u; is_counter c γ n q ∗ ⌜n ≤ u⌝ }}}.
+Proof.
+Admitted.
+
+Lemma read_spec_full (c : val) (γ : gname) (n : nat) :
+  {{{ is_counter c γ n 1 }}} read c {{{ RET #n; is_counter c γ n 1 }}}.
+Proof.
+Admitted.
+
+Lemma incr_spec (c : val) (γ : gname) (n : nat) (q : Qp) :
+  {{{ is_counter c γ n q }}}
+    incr c
+  {{{ (u : nat), RET #u; ⌜n ≤ u⌝ ∗ is_counter c γ (S n) q }}}.
+Proof.
+  iIntros "%Φ (%l & -> & Hγ' & #I) HΦ".
+  iLöb as "IH".
+  wp_rec.
+  wp_bind (! _)%E.
+  iInv "I" as "(%m & Hl & Hγ)".
+  wp_load.
+  iModIntro.
+  iSplitL "Hl Hγ".
+  { iExists m. iFrame. }
+  wp_pures.
+  wp_bind (CmpXchg _ _ _).
+  iInv "I" as "(%m' & Hl & Hγ)".
+  destruct (decide (# m = # m')).
+  - injection e as e.
+    apply (inj Z.of_nat) in e.
+    subst m'.
+    wp_cmpxchg_suc.
+Admitted.
+
+End spec2.
+End spec2.
